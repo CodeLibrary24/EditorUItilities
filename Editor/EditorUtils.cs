8bb84f3f9f9fc4c57dd19a7499aa8274769e447e
CircusCharlie24 <circuscharlie2428@gmail.com>
2023-01-27 00:24:41 +0530

    Cleaning up the popup system

--- Editor/EditorUtils.cs
+++ Editor/EditorUtils.cs
@@ -19,44 +19,7 @@ namespace CircusCharlie.DataTaggingTool
                 .Select(AssetDatabase.LoadAssetAtPath<T>).ToList();
         }
 
-        public static void ShowConfirmationPopup(Action onConfirm, string message, string confirmButtonText,
-            string cancelButtonText, string heading, Action onCancel = null, bool showAtScreenCenter = false)
-        {
-            Rect windowPos = EditorWindow.focusedWindow.position;
-            ConfirmationPopupView popupViewContent = new ConfirmationPopupView(confirmButtonText, cancelButtonText);
-
-            popupViewContent.SetHeading(heading);
-            popupViewContent.SetOnConfirmAction(onConfirm);
-            popupViewContent.SetOnCancelAction(onCancel);
-            popupViewContent.SetMessage(message);
-
-            DataTaggingToolPopup.ShowPopup(windowPos, popupViewContent, showAtScreenCenter);
-        }
-
-        public static void ShowNotificationPopup(string heading, string message, Action onConfirm = null,
-            bool showAtScreenCenter = false)
-        {
-            Rect windowPos = new Rect();
-            if (EditorWindow.focusedWindow != null)
-            {
-                windowPos = EditorWindow.focusedWindow.position;
-            }
-
-            NotificationPopup notificationPopup = new NotificationPopup();
-            notificationPopup.SetMessage(message);
-            notificationPopup.SetHeading(heading);
-
-            notificationPopup.SetOnConfirmAction(onConfirm);
-
-            DataTaggingToolPopup.ShowPopup(windowPos, notificationPopup, showAtScreenCenter);
-        }
-
-        public static void ShowNewItemCreationPopup(Action<string> onConfirm, bool showAtScreenCenter = false)
-        {
-            Rect windowPos = EditorWindow.focusedWindow.position;
-            NewItemCreationPopup createItemPopup = new NewItemCreationPopup(onConfirm);
-            DataTaggingToolPopup.ShowPopup(windowPos, createItemPopup, showAtScreenCenter);
-        }
+      
 
 
         private static readonly List<int> PreviouslyDisabledUIElements = new List<int>();
