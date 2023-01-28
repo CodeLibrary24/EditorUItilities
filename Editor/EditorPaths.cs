8bae64434a0328cd5ad03ea2d0e0c28541abf03d
CircusCharlie24 <circuscharlie2428@gmail.com>
2023-01-28 13:26:16 +0530

    Fixing minor issue

--- Editor/EditorPaths.cs
+++ Editor/EditorPaths.cs
@@ -2,8 +2,8 @@
 {
     public static class EditorPaths
     {
-        // public cons string Root = "Assets";
-        public cons string Root = "com.codelibrary24.editorutilities";
+        // public const string Root = "Assets";
+        public const string Root = "com.codelibrary24.editorutilities";
         public const string BasePath = Root+"/EditorUtilities/Editor";
     }
 }
