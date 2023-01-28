6bdcc9116af8d60bd85af5b6dbb417d0d5ec4f5e
CircusCharlie24 <circuscharlie2428@gmail.com>
2023-01-28 13:28:14 +0530

    Fixing packages path

--- Editor/EditorPaths.cs
+++ Editor/EditorPaths.cs
@@ -3,7 +3,7 @@
     public static class EditorPaths
     {
         // public const string Root = "Assets";
-        public const string Root = "com.codelibrary24.editorutilities";
+        public const string Root = "Packages/com.codelibrary24.editorutilities";
         public const string BasePath = Root+"/EditorUtilities/Editor";
     }
 }
