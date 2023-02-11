1ae8279649332345e942f3dcbda0579fff597aee
CircusCharlie24 <circuscharlie2428@gmail.com>
2023-02-11 13:44:14 +0530

    Double clicking on node hub now opens its graph

--- Editor/CustomGraphView/Test_GraphView/GraphViewTestWindow.cs
+++ Editor/CustomGraphView/Test_GraphView/GraphViewTestWindow.cs
@@ -1,29 +1,41 @@
 using CodeLibrary24.EditorUtilities;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UIElements;
 
 public class GraphViewTestWindow : EditorWindow
 {
     private static readonly Vector2 MinWindowSize = new Vector2(1400, 800);
-    private CustomGraphView _graphView;
+    private static CustomGraphView _graphView;
     private CustomInspectorView _inspectorView;
     private NodeHub _selectedNodeHub;
     private NodeHubBarController _nodeHubBarController;
 
-    [FormerlySerializedAs("m_VisualTreeAsset")]
     [SerializeField]
-    private VisualTreeAsset mVisualTreeAsset = default;
+    private VisualTreeAsset m_VisualTreeAsset = default;
 
     [MenuItem("CodeLibrary24/EditorUtilities/Tests/Graph View")]
-    public static void ShowExample()
+    public static void OpenWindow()
     {
         GraphViewTestWindow window = GetWindow<GraphViewTestWindow>();
         window.minSize = MinWindowSize;
         window.titleContent = new GUIContent("GraphViewTestWindow");
     }
 
+    [OnOpenAsset]
+    public static bool OnOpenAsset(int instanceID, int line)
+    {
+        if (Selection.activeObject is NodeHub)
+        {
+            OpenWindow();
+            OnNodeHubSelected(Selection.activeObject as NodeHub);
+            return true;
+        }
+
+        return false;
+    }
+    
     private void OnEnable()
     {
         CustomGraphEventChannel.Instance.onClearViewsRequested += ClearViews;
@@ -38,6 +50,7 @@ public class GraphViewTestWindow : EditorWindow
 
     private void OnChangeDetected()
     {
+        
     }
 
     private void ClearViews()
@@ -47,7 +60,11 @@ public class GraphViewTestWindow : EditorWindow
 
     public void CreateGUI()
     {
-        mVisualTreeAsset.CloneTree(rootVisualElement);
+        if (m_VisualTreeAsset == null)
+        {
+            return;
+        }
+        m_VisualTreeAsset.CloneTree(rootVisualElement);
         _graphView = rootVisualElement.Q<CustomGraphView>();
         _inspectorView = rootVisualElement.Q<CustomInspectorView>();
         _nodeHubBarController = new NodeHubBarController(rootVisualElement, OnNodeHubSelected);
@@ -55,7 +72,7 @@ public class GraphViewTestWindow : EditorWindow
         _graphView.OnNodeViewSelected = OnNodeSelectionChanged;
     }
 
-    private void OnNodeHubSelected(NodeHub selectedNodeHub)
+    private static void OnNodeHubSelected(NodeHub selectedNodeHub)
     {
         _graphView.PopulateView(selectedNodeHub);
     }
