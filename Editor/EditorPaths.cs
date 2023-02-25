09cc0ba6a08697e86fb276f86768354db54f698d
CircusCharlie24 <circuscharlie2428@gmail.com>
2023-02-25 16:39:08 +0530

    Update EditorPaths.cs

--- Editor/EditorPaths.cs
+++ Editor/EditorPaths.cs
@@ -2,8 +2,8 @@
 {
     public static class EditorPaths
     {
-        public const string Root = "Assets/EditorUtilities";
-        // public const string Root = "Packages/com.codelibrary24.editorutilities";
+        // public const string Root = "Assets/EditorUtilities";
+        public const string Root = "Packages/com.codelibrary24.editorutilities";
         public const string BasePath = Root+"/Editor";
         public const string GraphViewStyleSheet = BasePath + "/CustomGraphView/UIDocuments/CustomGraphStyleSheet.uss";
         public const string NodeViewUxml = BasePath + "/CustomGraphView/UIDocuments/NodeView.uxml";
